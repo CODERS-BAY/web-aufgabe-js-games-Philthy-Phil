71b7e5d04f13fda16d1fb4714b457bcd4e1917d3
Philthy-Phil <phil.ziegelbauer@gmx.at>
2021-01-13 11:59:39 +0100

    hammurabi-quick-setup
    logic next

--- Hammurabi/_js/script.js
+++ Hammurabi/_js/script.js
@@ -3,8 +3,53 @@
 rules.addEventListener("click", gameRules);
 right.addEventListener("click", resetGame);
 btnclose.addEventListener("click", closeRules);
+btn.addEventListener("click", startLogic);
 
 
+var yearCount = 2021;
+var landPriceCount = 5;
+
+landPrice.innerHTML = landPriceCount;
+year.innerHTML = yearCount;
+
+
+
+
+function startLogic() {
+
+    gameEndYearCondition();
+    gameLandPrice();
+
+}
+
+
+
+function gameLandPrice() {
+    landPriceCount = Math.floor(Math.random() * 10) + 1;
+    if(yearCount > 2021) {
+        landPrice.innerHTML = landPriceCount;
+    }
+}
+
+function gameEndYearCondition() {
+    yearCount++;
+    year.innerHTML = yearCount;
+
+    if(yearCount == 2021 + 20) {
+        btn.classList.add("no-select");
+    }
+}
+
+
+feedCitzienAmount.oninput = function() {
+    outputFeedCitzienAmount.innerHTML = this.value;
+}
+farmCornAmount.oninput = function() {
+    outputFarmCornAmount.innerHTML = this.value;
+}
+realizeFarmLand.oninput = function() {
+    outputRealizeFarmLand.innerHTML = this.value;
+}
 
 
 
@@ -12,17 +57,11 @@ btnclose.addEventListener("click", closeRules);
 function gameRules() {
     box.classList.remove("dnone");
 }
+
 function closeRules() {
     box.classList.add("dnone");
 }
+
 function resetGame() {
     window.location.href=window.location.href;
-}
-function checkValidInput (x, y, z) {
-    if (isNaN(x) || isNaN(y) || isNaN(z) || 
-        x == "" || y == "" || z == "") {  
-        return false;
-    } else {
-        return true;
-    }
 }
